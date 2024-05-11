1a0ef0dc1afe8aad4ef363ea25057384a11627c5
Samia Senzuti <ssen0025@student.monash.edu>
2024-05-12 00:01:49 +1000

    crypto platform routes changed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
 
 const App = () => (
-  <div className="app">
-    <div className="navbar">
-      <Navbar />
-    </div>
-    <div className="main">
-      <Layout>
-        <div className="routes">
-          <Routes>
-            <Route path="/crypto-react" element={<Homepage />} />
-            <Route path="/exchanges" element={<Exchanges />} />
-            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-            <Route path="/news" element={<News />} />
-          </Routes>
+  <Router basename="/crypto-react">
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse Inc. <br />
+            All Rights Reserved.
+            <Link to="/">Home</Link>
+          </Typography.Title>
+          <Space>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
-      </Layout>
-      <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-          Cryptoverse Inc. <br />
-          All Rights Reserved.
-          <Link to="/">Home</Link>
-        </Typography.Title>
-        <Space>
-          <Link to="/exchanges">Exchanges</Link>
-          <Link to="/news">News</Link>
-        </Space>
       </div>
     </div>
-  </div>
+  </Router>
 );
 
 export default App;
